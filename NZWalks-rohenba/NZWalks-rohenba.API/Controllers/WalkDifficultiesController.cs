f987564c63d99ca12d7ed3e2df3242628ec9759d
Solvexgiga <solvexgiga@gmail.com>
2022-11-17 14:18:56 +0530

    End of Section 6

---
+++ NZWalks-rohenba/NZWalks-rohenba.API/Controllers/WalkDifficultiesController.cs
@@ -0,0 +1,110 @@
+﻿using AutoMapper;
+using Microsoft.AspNetCore.Mvc;
+using NZWalks_rohenba.API.Models.DTO;
+using NZWalks_rohenba.API.Repositories;
+
+namespace NZWalks_rohenba.API.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class WalkDifficultiesController : Controller
+    {
+        private readonly IWalkDifficultyRepository walkDifficultyRepository;
+        private readonly IMapper mapper;
+
+        public WalkDifficultiesController(IWalkDifficultyRepository walkDifficultyRepository, IMapper mapper)
+        {
+            this.walkDifficultyRepository = walkDifficultyRepository;
+            this.mapper = mapper;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetAllWalkDifficulties()
+        {
+            var walkDifficultiesDomain = await walkDifficultyRepository.GetAllAsync();
+
+            //Convert Domain to DTOs
+            var walkDifficultiesDTO = mapper.Map<List<Models.DTO.WalkDifficulty>>(walkDifficultiesDomain);
+             return Ok(walkDifficultiesDTO);           
+        }
+
+        [HttpGet]
+        [Route("{id:guid}")]
+        [ActionName("GetWalkDifficultyById")]
+        public async Task<IActionResult> GetWalkDifficultyById(Guid id)
+        {
+            var walkDifficulty = await walkDifficultyRepository.GetAsync(id);
+            if (walkDifficulty == null)
+            {
+                return NotFound();
+            }
+
+            //Convert Domain to DTOs
+            var walkDifficultyDTO = mapper.Map<Models.DTO.WalkDifficulty>(walkDifficulty);
+
+            return Ok(walkDifficultyDTO);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> AddWalkDifficultyAsync
+            (Models.DTO.AddWalkDifficultyRequest addWalkDifficultyRequest)
+        {
+            //convert DTO to Domain Model
+            var walkDifficultyDomain = new Models.Domain.WalkDifficulty
+            {
+                Code = addWalkDifficultyRequest.Code
+            };
+
+            // Call repository
+             walkDifficultyDomain = await walkDifficultyRepository.AddAsync(walkDifficultyDomain);
+
+            //Convert Domain to DTO
+           var  walkDifficultyDTO = mapper.Map<Models.DTO.WalkDifficulty>(walkDifficultyDomain);
+
+            // Return response
+            return CreatedAtAction(nameof(GetWalkDifficultyById), 
+                new { id = walkDifficultyDTO.Id }, walkDifficultyDTO);
+        }
+
+        [HttpPut]
+        [Route("{id:guid}")]
+        public async Task<IActionResult> UpdateWalkDifficultyAsync
+            (Guid id, Models.DTO.UpdateWalkDifficultyRequest updateWalkDifficultyRequest)
+        {
+            // convert DTO to Domain Model
+            var walkDifficultyDomain = new Models.Domain.WalkDifficulty
+            {
+                Code = updateWalkDifficultyRequest.Code
+            };
+
+            // Call repository to update
+            walkDifficultyDomain = await walkDifficultyRepository.UpdateAsync(id, walkDifficultyDomain);
+            if( walkDifficultyDomain == null )
+            {
+                return NotFound();
+            }
+
+            //convert Domain to DTO
+            var walkDifficultyDTO = mapper.Map<Models.DTO.WalkDifficulty>(walkDifficultyDomain);
+
+            //Return response 
+            return Ok(walkDifficultyDTO);
+        }
+
+
+        [HttpDelete]
+        [Route("{id:guid}")]
+        public async Task<IActionResult> DeleteWalkDifficulty(Guid id)
+        {
+            var walkDifficultyDomain = await walkDifficultyRepository.DeleteAsync(id);
+            if(walkDifficultyDomain == null)
+            {
+                return NotFound();
+            }
+            // convert to DTO
+            var walkDifficultyDTO = mapper.Map<Models.DTO.WalkDifficulty>(walkDifficultyDomain);
+            return Ok(walkDifficultyDTO);
+        }
+
+    }
+}
---
+++ NZWalks-rohenba/NZWalks-rohenba.API/Models/DTO/AddWalkDifficultyRequest.cs
@@ -0,0 +1,7 @@
+﻿namespace NZWalks_rohenba.API.Models.DTO
+{
+    public class AddWalkDifficultyRequest
+    {
+        public string Code { get; set; }
+    }
+}
